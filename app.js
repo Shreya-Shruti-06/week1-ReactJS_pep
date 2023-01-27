1c002dca7a4eb69c0c53b42a68044b41db2f0e5e
Shreya Shruti <93046270+Shreya-Shruti-06@users.noreply.github.com>
2023-01-27 22:08:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Test from "./components/Test";
+
+import AllUserList from "./components/AllUserList";
+const App = () => {
+  return(
+    <div>
+      {/* <Test  key = {1} /> */}
+      {/* <Test key = {2}/>
+      <Test key = {3}/>
+      <Test key = {4}/>
+      <Test key = {5}/>
+      <Test key={6}/> */}
+      <AllUserList/>
+    </div>
+  )
+}
+
+export default App;
